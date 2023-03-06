6cec469feb57592b0861dc3714bcd9a81a59875e
Dev Luan Lima <luan_marcell@outlook>
2023-03-06 20:05:05 -0300

    Rest of initial Configuration

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent {
+export class AlertComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
--- src/app/services/index.service.ts
+++ src/app/services/index.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
-import { environment } from '@environments/environment';
-
 
 @Injectable({
   providedIn: 'root'
 })
-const baseUrl = `${environment.apiUrl}/users`;
-
 export class IndexService {
 
   constructor() { }
